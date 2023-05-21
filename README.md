cc6dd8a9dfac4830e4d5b301774d8e70a701423f
lucasheck <lucasheck23@gmail.com>
2023-05-21 18:06:13 -0400

    1 - Installing dayjs 2 - Installing image-picker 3 - Adding todo on readme 4 - Saving new memories with API 5 - Listing memories on load 6 - Adding animation when change screen

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 expo start
+
+#todo
+
+Spinner
+Recarregar a listagem quando uma nova memória for criada
+Botão de detalhe da memória
+Minha Assinatura no rodapé da aplicação?
--- app/new.tsx
+++ app/new.tsx
@@ -5,18 +5,80 @@ import {
   Text,
   TextInput,
   ScrollView,
+  Image,
 } from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
 
 import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
 
 export default function New() {
+  const router = useRouter()
   const { bottom, top } = useSafeAreaInsets()
 
   const [isPublic, setIsPublic] = useState(false)
+  const [content, setContent] = useState('')
+  const [preview, setPreview] = useState('')
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch {}
+  }
+
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+      uploadFormData.append('file', {
+        name: 'image.jpg',
+        type: 'image/jpeg',
+        uri: preview,
+      } as any)
+
+      try {
+        const uploadResponse = await api.post('/upload', uploadFormData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        coverUrl = uploadResponse.data.fileUrl
+      } catch (error) {
+        console.log(error)
+      }
+
+      await api.post(
+        '/memories',
+        {
+          content,
+          isPublic,
+          coverUrl,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+
+      router.push('/memories')
+    }
+  }
 
   return (
     <ScrollView
@@ -43,17 +105,32 @@ export default function New() {
             Tornar memória pública
           </Text>
         </View>
-        <TouchableOpacity className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20">
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color="#fff" />
-            <Text className="font-body text-sm text-gray-200">
-              Adicionar foto ou vídeo de capa
-            </Text>
-          </View>
+
+        <TouchableOpacity
+          onPress={openImagePicker}
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+        >
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              alt="Image Selected"
+              className="h-full w-full rounded-lg object-cover"
+            ></Image>
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#fff" />
+              <Text className="font-body text-sm text-gray-200">
+                Adicionar foto ou vídeo de capa
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
+
         <TextInput
           multiline
-          className="p-0 font-body text-lg text-gray-50"
+          textAlignVertical="top"
+          className="pt-0 font-body text-lg text-gray-50"
+          onChangeText={setContent}
           placeholderTextColor="#56565a"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
@@ -61,6 +138,7 @@ export default function New() {
         <TouchableOpacity
           className="items-center rounded-full bg-green-500 px-5 py-3"
           activeOpacity={0.7}
+          onPress={handleCreateMemory}
         >
           <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
         </TouchableOpacity>
